11bef70cec231c1ee764df1a63094225cd56082f
sebq18 <sebas182005@gmail.com>
2026-06-27 17:19:55 -0600

    Creación base de la app

---
+++ DB_Proyecto/src/main/java/cr/ac/ucr/dds3/paraiso/db_proyecto/DB_Proyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cr.ac.ucr.dds3.paraiso.db_proyecto;
+
+/**
+ *
+ * @author sebas
+ */
+public class DB_Proyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
